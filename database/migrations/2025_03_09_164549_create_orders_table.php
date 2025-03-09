3b61ccc1c85c19af1aa1dbb1bfdd8c178809758e
Batraz2004 <batraz.tolasovv@gmail.com>
2025-03-09 21:04:27 +0300

    create migration,controller,model and edit view cart for orders

---
+++ database/migrations/2025_03_09_164549_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->decimal('price',8,2);
+            $table->decimal('total_price',8,2);
+            $table->enum('status', ['active', 'cancelled']);
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('goods_id')->constrained('goods');
+            $table->string('user_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
--- resources/views/cart/list.blade.php
+++ resources/views/cart/list.blade.php
@@ -11,9 +11,8 @@
                     <div class ="main__catalog-content-product product"> 
                             <div class="main__catalog-content-produc__inner">
                                 @foreach($cartItems as $item)
-                                    <Form class="product__form" name="product-form" method="POST" action="{{route("cartEdit")}}"> 
-                                    @csrf  
-                                        <div class="product__inner">
+                                <div class="product__form">
+                                    <div class="product__inner">
                                             <ul class="product__list" style="list-style:none;">
                                                 <li class="product__list-item product__name"><p class="product_list-item-text">{{$item['name']}}</p></li>
                                                 <li class="product__list-item product__descr"><p class="product_list-item-text">описание:{{$item['description']}}</p></li>
@@ -21,6 +20,10 @@
                                                 <li class="product__list-item product__price"><p class="product_list-item-text">кол-во:{{$item['quantity']}}</p></li>
 
                                             </ul>
+                                    </div>
+                                    <Form class="" name="product-form" method="POST" action="{{route("cartEdit")}}"> 
+                                        @csrf 
+                                        <div class="product__form-inner">
                                             <input type="hidden" name="itemId" value="{{$item['id']}}">
                                             <input type="hidden" name="goodsId" value="{{$item['goods_id']}}">
                                             <input type="hidden" name="itemName" value="{{$item['name']}}">
@@ -29,10 +32,24 @@
                                         </div>
                                         <div class="button__pannel">
                                             <button type="submit" class="catalog__button" name="submit" value="complete-order-id"><p>оформить заказ</p></button>
+                                        </div>
+                                    </Form>
+                                    <Form class="" name="product-form" method="POST" action="{{route("cartEdit")}}"> 
+                                        @csrf  
+                                        
+                                        <div class="product__form-inner">
+                                            <input type="hidden" name="itemId" value="{{$item['id']}}">
+                                            <input type="hidden" name="goodsId" value="{{$item['goods_id']}}">
+                                            <input type="hidden" name="itemName" value="{{$item['name']}}">
+                                            <input type="hidden" name="itemPrice" value="{{$item['price']}}">
+                                            <input type="hidden" name="itemCount" value="{{$item['quantity']}}">
+                                        </div>
+                                        <div class="button__pannel">
                                             <button type="submit" class="catalog__button" name="submit" value="detail-id"><p>подробнее</p></button>
                                             <button type="submit" class="catalog__button" name="submit" value="delete-id"><p>удалить</p></button>
                                         </div>
                                     </Form>
+                                </div>
                                 @endforeach
                             </div>
                     </div>
